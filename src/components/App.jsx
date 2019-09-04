b2c57203683a5f606c0d71e50a811bdd74862048
bcauchon <bcauchon@gmail.com>
2019-09-04 16:18:44 -0700

    Add stylesheet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import MyStyledComponent from './MyStyledComponent';
+import './styles.css';
 
 function App(){
   return (
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Ticket(props){
   return (
-    <div>
+    <div className='ticket'>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>
